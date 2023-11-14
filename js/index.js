7c0ea4f81c66fd4b54b57ecb6b7845d30d58a01a
unknown <eymeric57160@hotmail.fr>
2023-11-14 19:52:53 +0100

    img english

---
+++ js/index.js
@@ -0,0 +1,62 @@
+const rules = document.getElementById("rules");
+const true1 = document.getElementById("true");
+const false1 = document.getElementById("false");
+const main1 = document.getElementById("main1");
+const close = document.getElementsByTagName(":after");
+
+rules.addEventListener("click", () => {
+  rules1.style.transform = "translateX(0)";
+  main1.style.filter = "blur(5px)";
+ 
+});
+
+close1.addEventListener("click", () => {
+  rules1.style.transform = "translateY(-2000px)";
+  main1.style.filter = "blur(0px)";
+});
+
+bt1.addEventListener("click", () => {
+  bt1.style.border = " solid green 5px";
+  true1.style.opacity = "1";
+  next.style.transform = "translateX(0)";
+  next.style.opacity = "1";
+  bt2.style.display = "none";
+  bt3.style.display = "none";
+  bt4.style.display = "none";
+  retry.style.display = "none";
+});
+
+bt2.addEventListener("click", () => {
+  bt2.style.border = " solid red 5px";
+  retry.style.transform = "translateX(0)";
+  false1.style.opacity = "1";
+  bt1.style.display = "none";
+  bt3.style.display = "none";
+  bt4.style.display = "none";
+});
+
+bt3.addEventListener("click", () => {
+  bt3.style.border = " solid red 5px";
+  retry.style.transform = "translateX(0)";
+  false1.style.opacity = "1";
+  bt1.style.display = "none";
+  bt2.style.display = "none";
+  bt4.style.display = "none";
+});
+
+bt4.addEventListener("click", () => {
+  bt4.style.border = " solid red 5px";
+  retry.style.transform = "translateX(0)";
+  false1.style.opacity = "1";
+  bt1.style.display = "none";
+  bt3.style.display = "none";
+  bt2.style.display = "none";
+});
+
+retry.addEventListener("click", () => {
+  window.location.reload();
+});
+
+next.addEventListener("click", () => {
+  document.location.href = "./page2.html";
+});
--- js/index3.js
+++ js/index3.js
@@ -1,53 +1,67 @@
 const rules = document.getElementById('rules')
 const true1 = document.getElementById('true')
 const false1 = document.getElementById('false')
-
+const main1 = document.getElementById('main1')
+const close = document.getElementsByTagName(':after')
 
 rules.addEventListener('click',() => {
-    alert("choisis le bon nom de l'animal !");
-})
-
-
-
+    
+    rules1.style.transform="translateX(0)"
+       main1.style.filter = "blur(5px)" 
+    })
+    
+    close1.addEventListener('click' ,() => {
+        rules1.style.transform="translateY(-2000px)"
+        main1.style.filter = "blur(0px)" 
+    
+        
+    })
+    
+    
 
 bt1.addEventListener('click', () => {
-    bt1.style.border =" solid green 5px";
-    true1.style.opacity ="1";
-    next.style.opacity ="1"
+    bt1.style.border ="solid red 5px";
+    false1.style.opacity ="1";
+   
     bt2.style.display ="none"
     bt3.style.display ="none"
     bt4.style.display ="none"
-    retry.style.display ="none"
+    retry.style.transform ="translateX(0)"
 
 })
 
 bt2.addEventListener('click', () => {
     bt2.style.border =" solid red 5px"
-    retry.style.opacity ="1"
     false1.style.opacity ="1";
     bt1.style.display ="none"
     bt3.style.display ="none"
     bt4.style.display ="none"
+    retry.style.transform ="translateX(0)"
 })
 
 bt3.addEventListener('click', () => {
     bt3.style.border =" solid red 5px"
-    retry.style.opacity ="1"
     false1.style.opacity ="1";
     bt1.style.display ="none"
     bt2.style.display ="none"
     bt4.style.display ="none"
+    retry.style.transform ="translateX(0)"
 })
 
 bt4.addEventListener('click', () => {
-    bt4.style.border =" solid red 5px"
-    retry.style.opacity ="1"
-    false1.style.opacity ="1";
+    bt4.style.border =" solid green 5px"
+    true1.style.opacity ="1";
     bt1.style.display ="none"
     bt3.style.display ="none"
     bt2.style.display ="none"
+    next.style.transform ="translateX(0)"
 })
 
 retry.addEventListener('click',() => {
     window.location.reload()
+})
+
+next.addEventListener('click',() => {
+    document.location.href='../html/page4.html'
+    
 })
